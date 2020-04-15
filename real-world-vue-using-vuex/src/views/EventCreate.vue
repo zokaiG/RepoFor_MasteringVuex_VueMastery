3a6b60aa7dd40546da248326ee4d677f97f9931b
zokaiG <futlouco7@gmail.com>
2020-04-15 11:14:38 -0400

    Completed Lesson 7 - Success & Error Notifications

--- real-world-vue-using-vuex/src/views/EventCreate.vue
+++ real-world-vue-using-vuex/src/views/EventCreate.vue
@@ -64,9 +64,7 @@ export default {
           })
           this.event = this.createFreshEvent()
         })
-        .catch(() => {
-          console.log('There was a problem creating your event')
-        })
+        .catch(() => {})
     },
     createFreshEvent() {
       const user = this.$store.state.user.user
