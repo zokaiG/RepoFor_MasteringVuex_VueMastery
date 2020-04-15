f942351201ee5bce74a66678b74dc872d1eea936
zokaiG <futlouco7@gmail.com>
2020-04-15 10:04:53 -0400

    Completed Lesson 6 - Modules

--- real-world-vue-using-vuex/src/views/EventList.vue
+++ real-world-vue-using-vuex/src/views/EventList.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <h1>Events Listing</h1>
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
+    <h1>Events for {{ user.user.name }}</h1>
+    <EventCard v-for="event in event.events" :key="event.id" :event="event" />
     <template v-if="page != 1">
       <router-link :to="{name: 'event-list', query: {page: page - 1}}" rel="prev">Prev Page</router-link>
     </template>
@@ -20,19 +20,19 @@ export default {
     EventCard
   },
   created() {
-    this.$store.dispatch('fetchEvents', {
+    this.$store.dispatch('event/fetchEvents', {
       perPage: 3,
       page: this.page
     })
   },
   computed: {
     isMaxPage() {
-      return this.totalEvents / 3 > this.page ? true : false
+      return this.event.totalEvents / 3 > this.page ? true : false
     },
     page() {
       return parseInt(this.$route.query.page) || 1
     },
-    ...mapState(['events', 'totalEvents'])
+    ...mapState(['event', 'user'])
   }
 }
 </script>
